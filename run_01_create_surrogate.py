6efcf0aeb17dfd4929c4c067aae4bb4408bf63d1
Yong Hoon Lee <yonghoonlee@outlook.com>
2024-02-26 09:49:49 -0600

    visualize rsm

--- run_01_create_surrogate.py
+++ run_01_create_surrogate.py
@@ -25,14 +25,24 @@ sm.set_training_values(xt, yt)
 sm.train()
 
 # Plot
-fg = plt.figure()
-N = 31
+N = 51
+fg, ax = plt.subplots(nrows=1, ncols=2)
+fg.set_figwidth(10.0)
+fg.set_figheight(4.5)
 [X, Y] = np.meshgrid(np.linspace(RANGE[0,0], RANGE[1,0], N), np.linspace(RANGE[0,1], RANGE[1,1], N))
 I = np.concatenate((np.reshape(X, (-1,1)), np.reshape(Y, (-1,1))), axis=1)
 R = sm.predict_values(I)
-plt.contourf(X, Y, np.reshape(R[:,0], (N,N)))
-plt.plot(xt[:,0], xt[:,1], 'ko')
-plt.show()
+ctr0 = ax[0].contourf(X, Y, np.reshape(R[:,0], (N,N)), levels=10)
+#pdt0 = ax[0].plot(xt[:,0], xt[:,1], 'ko')
+ax[0].set_xlabel('Rotor-area water velocity [m/s]')
+ax[0].set_ylabel('Blade pitch angle} [deg]')
+fg.colorbar(ctr0, ax=ax[0])
+ctr1 = ax[1].contourf(X, Y, np.reshape(R[:,1], (N,N)), levels=10)
+#pdt1 = ax[1].plot(xt[:,0], xt[:,1], 'ko')
+ax[1].set_xlabel('Rotor-area water velocity [m/s]')
+ax[1].set_ylabel('Blade pitch angle} [deg]')
+fg.colorbar(ctr1, ax=ax[1])
+fg.show()
 
 prob = om.Problem()
 prob.model = om
