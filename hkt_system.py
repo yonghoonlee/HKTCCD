95d0e01742f11e7a93e62b9d046974f57053ebdc
Yong Hoon Lee <yonghoonlee@outlook.com>
2024-02-26 16:59:02 -0600

    basic control design implemented

--- hkt_system.py
+++ hkt_system.py
@@ -10,6 +10,7 @@ class hkt_generator(om.ExplicitComponent):
         self.options.declare('sm')
         self.options.declare('V_min')
         self.options.declare('V_max')
+
     
     def setup(self):
 
@@ -25,13 +26,24 @@ class hkt_generator(om.ExplicitComponent):
         self.add_input('V_water', shape=(nn,), units='m/s')
 
         # control variables
-        self.add_input('pitch', shape=(nn,), units='rad')
+        self.add_input('pitch', shape=(nn,), units='deg')
         self.add_input('duct_contraction_ratio', shape=(nn,), units=None)
 
         # outputs
         self.add_output('V_throat', shape=(nn,), units='m/s')
-        self.add_output('power', shape=(nn,), units='W')
+        self.add_output('effective_duct_contraction_ratio', shape=(nn,), units=None)
+        self.add_output('power', shape=(nn,), units='mW')
         self.add_output('omega', shape=(nn,), units='rad/s')
+        self.add_output('avg_power', shape=(1,), units='mW')
+
+
+    def setup_partials(self):
+        self.declare_partials('V_throat', ['V_water', 'duct_contraction_ratio'], method='fd')
+        self.declare_partials('effective_duct_contraction_ratio', ['V_water', 'duct_contraction_ratio'], method='fd')
+        self.declare_partials('power', ['V_water', 'duct_contraction_ratio', 'pitch'], method='fd')
+        self.declare_partials('omega', ['V_water', 'duct_contraction_ratio', 'pitch'], method='fd')
+        self.declare_partials('avg_power', ['V_water', 'duct_contraction_ratio', 'pitch', 't'], method='fd')
+
 
     def compute(self, inputs, outputs):
 
@@ -51,10 +63,12 @@ class hkt_generator(om.ExplicitComponent):
 
         R = sm.predict_values(np.concatenate((effective_duct_contraction_ratio.reshape((-1, 1)), pitch.reshape((-1, 1))), axis=1))
         power = R[:,0].flatten()
-        omega = R[:,1].flatten()
+        omega = R[:,1].flatten()*(2.0*np.pi/60.0)
 
         # outputs
         outputs['V_throat'] = V_throat
+        outputs['effective_duct_contraction_ratio'] = effective_duct_contraction_ratio
         outputs['omega'] = omega
         outputs['power'] = power
+        outputs['avg_power'] = np.sum(power)/t[-1]
 
--- run_ccd.py
+++ run_ccd.py
@@ -1,7 +1,6 @@
 import numpy as np
 from smt.surrogate_models import RMTC
 import csv
-from matplotlib import pyplot as plt
 import openmdao.api as om
 from hkt_system import hkt_generator
 
@@ -29,49 +28,80 @@ sm.train()
 N = 51
 V_min = 0.18
 V_max = V_min/np.min(xt[:,0])**2
-[X, Y] = np.meshgrid(np.linspace(RANGE[0,0], RANGE[1,0], N), np.linspace(RANGE[0,1], RANGE[1,1], N))
-I = np.concatenate((np.reshape(X, (-1,1)), np.reshape(Y, (-1,1))), axis=1)
-R = sm.predict_values(I)
-fg0, ax0 = plt.subplots(nrows=2, ncols=2)
-fg0.set_figwidth(12.0)
-fg0.set_figheight(9.0)
-# plot 1
-ctr0 = ax0[0,0].contourf(X, Y, np.reshape(R[:,0], (N,N)), levels=10)
-#pdt0 = ax0[0,0].plot(xt[:,0], xt[:,1], 'ko')
-ax0[0,0].set_xlabel('Duct contraction ratio [-]')
-ax0[0,0].set_ylabel('Blade pitch angle [deg]')
-ax0[0,0].set_title('Power [mW]')
-fg0.colorbar(ctr0, ax=ax0[0,0])
-# plot 2
-ctr1 = ax0[0,1].contourf(X, Y, np.reshape(R[:,1], (N,N))*(2.0*np.pi/60.0), levels=10)
-#pdt1 = ax0[0,1].plot(xt[:,0], xt[:,1], 'ko')
-ax0[0,1].set_xlabel('Duct contraction ratio [-]')
-ax0[0,1].set_ylabel('Blade pitch angle [deg]')
-ax0[0,0].set_title('Angular velocity [rad/s]')
-fg0.colorbar(ctr1, ax=ax0[0,1])
-# plot 3
-ctr2 = ax0[1,0].contourf(V_min/(X*X), Y, np.reshape(R[:,0], (N,N)), levels=10)
-#pdt2 = ax0[1,0].plot(V_min/(xt[:,0]*xt[:,0]), xt[:,1], 'ko')
-ax0[1,0].set_xlabel('Rotor area water velocity [m/s]')
-ax0[1,0].set_ylabel('Blade pitch angle [deg]')
-ax0[1,0].set_title('Power [mW]')
-fg0.colorbar(ctr2, ax=ax0[1,0])
-# plot 4
-ctr3 = ax0[1,1].contourf(V_min/(X*X), Y, np.reshape(R[:,1], (N,N))*(2.0*np.pi/60.0), levels=10)
-#pdt3 = ax0[1,1].plot(V_min/(xt[:,0]*xt[:,0]), xt[:,1], 'ko')
-ax0[1,1].set_xlabel('Rotor area water velocity [m/s]')
-ax0[1,1].set_ylabel('Blade pitch angle [deg]')
-ax0[1,1].set_title('Angular velocity [rad/s]')
-fg0.colorbar(ctr3, ax=ax0[1,1])
-fg0.tight_layout()
-fg0.savefig('fig_power_omega.pdf', format='pdf')
+PLOT = False
+
+if PLOT:
+    from matplotlib import pyplot as plt
+    [X, Y] = np.meshgrid(np.linspace(RANGE[0,0], RANGE[1,0], N), np.linspace(RANGE[0,1], RANGE[1,1], N))
+    I = np.concatenate((np.reshape(X, (-1,1)), np.reshape(Y, (-1,1))), axis=1)
+    R = sm.predict_values(I)
+    fg0, ax0 = plt.subplots(nrows=2, ncols=2)
+    fg0.set_figwidth(12.0)
+    fg0.set_figheight(9.0)
+    # plot 1
+    ctr0 = ax0[0,0].contourf(X, Y, np.reshape(R[:,0], (N,N)), levels=10)
+    pdt0 = ax0[0,0].plot(xt[:,0], xt[:,1], 'ko')
+    ax0[0,0].set_xlabel('Duct contraction ratio [-]')
+    ax0[0,0].set_ylabel('Blade pitch angle [deg]')
+    ax0[0,0].set_title('Power [mW]')
+    fg0.colorbar(ctr0, ax=ax0[0,0])
+    # plot 2
+    ctr1 = ax0[0,1].contourf(X, Y, np.reshape(R[:,1], (N,N))*(2.0*np.pi/60.0), levels=10)
+    #pdt1 = ax0[0,1].plot(xt[:,0], xt[:,1], 'ko')
+    ax0[0,1].set_xlabel('Duct contraction ratio [-]')
+    ax0[0,1].set_ylabel('Blade pitch angle [deg]')
+    ax0[0,1].set_title('Angular velocity [rad/s]')
+    fg0.colorbar(ctr1, ax=ax0[0,1])
+    # plot 3
+    ctr2 = ax0[1,0].contourf(V_min/(X*X), Y, np.reshape(R[:,0], (N,N)), levels=10)
+    #pdt2 = ax0[1,0].plot(V_min/(xt[:,0]*xt[:,0]), xt[:,1], 'ko')
+    ax0[1,0].set_xlabel('Rotor area water velocity [m/s]')
+    ax0[1,0].set_ylabel('Blade pitch angle [deg]')
+    ax0[1,0].set_title('Power [mW]')
+    fg0.colorbar(ctr2, ax=ax0[1,0])
+    # plot 4
+    ctr3 = ax0[1,1].contourf(V_min/(X*X), Y, np.reshape(R[:,1], (N,N))*(2.0*np.pi/60.0), levels=10)
+    #pdt3 = ax0[1,1].plot(V_min/(xt[:,0]*xt[:,0]), xt[:,1], 'ko')
+    ax0[1,1].set_xlabel('Rotor area water velocity [m/s]')
+    ax0[1,1].set_ylabel('Blade pitch angle [deg]')
+    ax0[1,1].set_title('Angular velocity [rad/s]')
+    fg0.colorbar(ctr3, ax=ax0[1,1])
+    fg0.tight_layout()
+    fg0.savefig('fig_power_omega.pdf', format='pdf')
 
 # Problem definition
-nn = 6001   # nn=6001 for 10 minutes with dt=0.1s
-dt = 0.1    # dt=0.1s
+nn = 61    # nn=61 for 1 minutes with dt=1.0s
+dt = 1.0    # dt=1.0s
 prob = om.Problem()
 prob.model = om.Group()
 prob.model.add_subsystem(name='hkt_generator', subsys=hkt_generator(num_nodes=nn, dt=dt, sm=sm, V_min=V_min, V_max=V_max), promotes=['*'])
+prob.model.add_subsystem(name='hkt_lackofdata', subsys=om.ExecComp(
+        'data_feasibility = 2.0*effective_duct_contraction_ratio + 0.25*pitch - 3.25',
+        data_feasibility = {'shape': (nn,), 'units': None},
+        effective_duct_contraction_ratio = {'shape': (nn,), 'units': None},
+        pitch = {'shape': (nn,), 'units': 'deg'},
+        do_coloring=False,
+    ),
+    promotes=['*'])
+
+prob.model.add_design_var(name='duct_contraction_ratio', lower=0.5, upper=1.0)
+prob.model.add_design_var(name='pitch', lower=-12.0, upper=10.0)
+prob.model.add_constraint('effective_duct_contraction_ratio', lower=0.5, upper=1.0)
+prob.model.add_constraint('pitch', lower=-12.0, upper=10.0)
+prob.model.add_constraint('data_feasibility', lower=None, upper=0.0)
+prob.model.add_objective('avg_power', ref=-1)
+
+# prob.driver = om.ScipyOptimizeDriver()
+# prob.driver.options['optimizer'] = 'SLSQP'
+# prob.driver.options['tol'] = 1e-3
+# prob.driver.options['disp'] = True
+# prob.driver.opt_settings['eps'] = 1e-4
+
+prob.driver = om.pyOptSparseDriver()
+prob.driver.options['optimizer'] = 'IPOPT'
+prob.driver.options['print_results'] = True
+prob.driver.opt_settings['print_level'] = 5
+
 
 prob.setup()
 t = np.linspace(0.0, float(nn-1)*dt, nn)
@@ -80,7 +110,8 @@ prob.set_val('t', val=t)
 prob.set_val('V_water', val=V_water)
 prob.set_val('pitch', np.zeros(t.shape))
 prob.set_val('duct_contraction_ratio', np.ones(t.shape))
-prob.run_model()
+#prob.run_model()
+prob.run_driver()
 
 power = prob.get_val('power')
 omega = prob.get_val('omega')
