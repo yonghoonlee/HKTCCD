09404da6a670d419129ef7667bd531f91c808efa
Yong Hoon Lee <yonghoonlee@outlook.com>
2024-02-26 14:58:22 -0600

    actual throat velocity calculation

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 
 ```
-mamba create --name hktccd python dymos numpy scipy smt yaml matplotlib numba
+mamba create --name hktccd python dymos numpy scipy smt yaml matplotlib numba pyoptsparse
 git clone https://github.com/yonghoonlee/HKTCCD.git
 
 ```
--- hkt_system.py
+++ hkt_system.py
@@ -8,12 +8,16 @@ class hkt_generator(om.ExplicitComponent):
         self.options.declare('num_nodes', types=int)
         self.options.declare('dt', types=float)
         self.options.declare('sm')
+        self.options.declare('V_min')
+        self.options.declare('V_max')
     
     def setup(self):
 
         nn = self.options['num_nodes']
         dt = self.options['dt']
         sm = self.options['sm']
+        V_min = self.options['V_min']
+        V_max = self.options['V_max']
         t = np.linspace(0.0, float(nn-1)*dt, nn)
 
         # given inputs
@@ -26,28 +30,28 @@ class hkt_generator(om.ExplicitComponent):
 
         # outputs
         self.add_output('V_throat', shape=(nn,), units='m/s')
-        self.add_output('omega', shape=(nn,), units='rad/s')
         self.add_output('power', shape=(nn,), units='W')
-
-
+        self.add_output('omega', shape=(nn,), units='rad/s')
 
     def compute(self, inputs, outputs):
 
         nn = self.options['num_nodes']
+        dt = self.options['dt']
+        sm = self.options['sm']
+        V_min = self.options['V_min']
+        V_max = self.options['V_max']
 
         # inputs
         t = inputs['t']
         V_water = inputs['V_water']
         pitch = inputs['pitch']
         duct_contraction_ratio = inputs['duct_contraction_ratio']
-
         V_throat = V_water / duct_contraction_ratio**2
-        omega =  
+        effective_duct_contraction_ratio = np.sqrt(V_min / V_throat)
 
-
-
-
-
+        R = sm.predict_values(np.concatenate((effective_duct_contraction_ratio.reshape((-1, 1)), pitch.reshape((-1, 1))), axis=1))
+        power = R[:,0].flatten()
+        omega = R[:,1].flatten()
 
         # outputs
         outputs['V_throat'] = V_throat
